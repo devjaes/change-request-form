e74ae107ff23da817b5ae32a577ce579a08179d6
emilia <emi1213galarza@gmail.com>
2023-11-29 22:18:47 -0500

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import JoinDashboard from "@/components/join-dashboard";
 
 export default async function Index() {
   return (
-    <div className="flex-1 w-full  h-11 flex flex-col gap-15 items-center">
+    <div className="flex-1 w-full flex flex-col gap-15 items-center">
       <div className="animate-in flex-1 flex flex-col gap-15 opacity-0 max-w-2xl px-1 items-center justify-center h-1/2">
         <main className="flex-1 flex flex-col gap-4 justify-center items-center">
           <h2 className="font-bold text-4xl mb-4">WELCOME</h2>
