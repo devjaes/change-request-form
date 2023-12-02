2a1075d3f46990b95f48cacb4a957aa54457ab5c
SrPabvliss <pablomartinvillacres@gmail.com>
2023-12-02 16:50:55 -0500

    ChetWode blue palette created in tailwind config

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -83,7 +83,7 @@ export default function Login({
           Email
         </label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          className="rounded-md px-4 py-2 bg-inherit border mb-6 "
           name="email"
           placeholder="you@example.com"
           required
